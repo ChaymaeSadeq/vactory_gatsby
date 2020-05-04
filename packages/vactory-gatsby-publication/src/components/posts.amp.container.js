f3fd0eebdd2c9ff810642c98b7ef100dbbd42b3c
mouad goulman <moadgoulman@gmail.com>
2020-05-04 16:39:26 +0000

    add publication plugin

---
+++ packages/vactory-gatsby-publication/src/components/posts.amp.container.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const PostsAMPContainer = ({pageContext: {posts}}) => {
+    return (<h1>AMP PUBLICATIONS - LIST</h1>)
+}
+
+export default PostsAMPContainer
---
+++ packages/vactory-gatsby-publication/src/internal/api.post.params.js
@@ -0,0 +1,10 @@
+// Node detail.
+export const params = {
+    fields: {
+        "node--publication": "drupal_internal__nid,langcode,title,path,field_vactory_meta_tags,created,field_vactory_date,field_vactory_excerpt,field_vactory_media_image,field_vactory_taxonomy_1,field_vactory_media_file,body",
+        "media--image": "thumbnail",
+        "file--image": "uri",
+        "taxonomy_term--publication_theme": "name",
+    },
+    include: "field_vactory_media_image,field_vactory_media_file,field_vactory_media_image.thumbnail,field_vactory_taxonomy_1",
+};
