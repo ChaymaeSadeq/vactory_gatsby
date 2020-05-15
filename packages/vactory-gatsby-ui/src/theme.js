fe998f5ac434d2d1aa70a48c76c4bb6a657a2da3
Hamza Bahlaouane <h.bahlaouane@void.fr>
2020-05-15 00:17:06 +0000

    Theme config.

--- packages/vactory-gatsby-ui/src/theme.js
+++ packages/vactory-gatsby-ui/src/theme.js
@@ -1,3 +1,26 @@
-export const theme = {
+const breakpoints = [
+    '500px',
+    '960px',
+    '1280px',
+    '1920px'
+];
 
+breakpoints.md = '960px';
+breakpoints.sm = '500px';
+breakpoints.xs = '0px';
+breakpoints.lg = '1280px';
+breakpoints.xl = '1920px';
+
+
+const buttons = {
+    fill: {
+        success : {
+            backgroundColor: 'success500',
+        },
+    }
+};
+
+export const theme = {
+    breakpoints,
+    buttons
 };
