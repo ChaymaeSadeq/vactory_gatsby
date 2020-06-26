afdf79f1f7a339e963503b7673c316101fd9eefb
Hamza Bahlaouane <h.bahlaouane@void.fr>
2020-06-26 09:51:31 +0100

    Gov: expose DF.

--- packages/vactory-gatsby-publication/src/components/posts.js
+++ packages/vactory-gatsby-publication/src/components/posts.js
@@ -3,7 +3,6 @@ import { Publication } from "vactory-gatsby-publication";
 import { Col, Pagination, Box, Container, Row } from "vactory-ui";
 
 const Posts = ({ posts, handlePaginationChange, pager, count }) => {
-  console.log(count);
   return (
     <div>
       <Container>
