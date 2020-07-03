2dc0519163f3a30777333dfaa183e7dee06586fc
Hamza Bahlaouane <h.bahlaouane@void.fr>
2020-07-03 14:48:13 +0100

    reCaptcha keys

--- projects/vactory/src/config/api-keys.js
+++ projects/vactory/src/config/api-keys.js
@@ -1,4 +1,4 @@
 export const CONFIG_API_KEYS = {
-    reCaptcha: "6LeFj98UAAAAAOkhk1iDNHJN073gLrnPduOv10QZ",
+    reCaptcha: "6Ler_6wZAAAAAAKp0S3DXOTNqIZcWqWWa2nOZLCV",
     gmap: "AIzaSyC6_thM-oXu2GGnlNHbQ0w0lBHXBeKPF8w",
 };
