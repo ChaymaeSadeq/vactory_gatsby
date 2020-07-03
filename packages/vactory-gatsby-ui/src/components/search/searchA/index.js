ac2fce64cd1bdbc157a5912e5e26cada2848bfa2
Hamza Bahlaouane <h.bahlaouane@void.fr>
2020-07-03 19:38:32 +0100

    SSR.

--- packages/vactory-gatsby-ui/src/components/search/searchA/index.js
+++ packages/vactory-gatsby-ui/src/components/search/searchA/index.js
@@ -1,10 +1,17 @@
-import React from "react"
-// import {Box, SearchOverlay, Button} from "vactory-ui";
+import React, {useState, useEffect} from "react"
 import {Box, SearchOverlay, Icon} from "vactory-ui";
 import {navigate} from "gatsby"
+import isClient from "is-client"
 
 export const SearchA = () => {
-    const [show, setShow] = React.useState(false);
+    const [show, setShow] = useState(false);
+    const [isBrowser, setIsBrowser] = useState(false);
+
+    // Check SSR
+    useEffect(() => {
+        setIsBrowser(isClient())
+    }, [isClient]); // eslint-disable-line react-hooks/exhaustive-deps
+
     return (
         <Box>
             <Box onClick={() => setShow(true)}
@@ -17,12 +24,14 @@ export const SearchA = () => {
                     size="large"
                 />
             </Box>
+            {isBrowser &&
             <SearchOverlay onSubmit={q => {
                 navigate('/fr/recherche?q=' + q);
                 setShow(false)
             }}
                            onClose={() => setShow(false)}
                            open={show}/>
+            }
         </Box>
     )
 };
