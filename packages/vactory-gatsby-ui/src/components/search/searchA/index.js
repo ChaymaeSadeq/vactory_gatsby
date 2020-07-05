b88eb363fd8a939e67ad5a2267c236c18648fc6c
Hamza Bahlaouane <h.bahlaouane@void.fr>
2020-07-05 18:24:54 +0100

    Search: useAlias

--- packages/vactory-gatsby-ui/src/components/search/searchA/index.js
+++ packages/vactory-gatsby-ui/src/components/search/searchA/index.js
@@ -2,10 +2,12 @@ import React, {useState, useEffect} from "react"
 import {Box, SearchOverlay, Icon} from "vactory-ui";
 import {navigate} from "gatsby"
 import isClient from "is-client"
+import {useViewsAlias} from "vactory-gatsby-nodes";
 
 export const SearchA = () => {
     const [show, setShow] = useState(false);
     const [isBrowser, setIsBrowser] = useState(false);
+    const searchUrl = useViewsAlias('search');
 
     // Check SSR
     useEffect(() => {
@@ -26,7 +28,7 @@ export const SearchA = () => {
             </Box>
             {isBrowser &&
             <SearchOverlay onSubmit={q => {
-                navigate('/fr/recherche?q=' + q);
+                navigate(`${searchUrl}?q=${q}`);
                 setShow(false)
             }}
                            onClose={() => setShow(false)}
