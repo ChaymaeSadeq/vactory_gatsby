36a0f86c3279d0b4edb6ce8d449e3b51e15254d1
Hamza Bahlaouane <h.bahlaouane@void.fr>
2020-07-06 10:29:14 +0100

    Override layout for vactory project

---
+++ projects/vactory/src/vactory-gatsby-ui/layouts/DefaultLayout.js
@@ -0,0 +1,26 @@
+import React from "react"
+import {
+    HeaderA as Header,
+    FooterA as Footer,
+    BannersTemplate,
+    CookieComplianceLayer
+} from 'vactory-gatsby-ui'
+
+export const DefaultLayout = ({children, location, pageContext: {node, pageInfo, breadcrumb}}) => {
+    return (
+        <>
+            <Header pageInfo={pageInfo} currentLanguage={node.langcode}/>
+            {node.internal_node_banner &&
+            <BannersTemplate
+                widget={node.internal_node_banner}
+                node={node}
+                breadcrumbItems={(breadcrumb && breadcrumb.length > 0) ? breadcrumb : [] }/>
+            }
+            <main>
+                {children}
+            </main>
+            <Footer/>
+            <CookieComplianceLayer />
+        </>
+    )
+}
