e1905c4ef79016447fb119a2d8c6585de8bc6b86
Hamza Bahlaouane <h.bahlaouane@void.fr>
2020-07-06 23:09:44 +0100

    Language detection: missing prefix for undefined pages.

--- packages/vactory-gatsby-core/src/utils/language.js
+++ packages/vactory-gatsby-core/src/utils/language.js
@@ -1,11 +1,11 @@
 export const getLanguageByPathname = (pathname) => {
-    let found;
-    const language = pathname.match(/\/([a-zA-Z-]*)/g);
+    let found = 'fr'; // @todo: get default value from config.
+    const language = pathname.match(/^\/[a-z]{2}\//g);
     if (language instanceof Array) {
         if (typeof language[0] !== 'string') {
             return undefined;
         }
-        found = language[0].replace('/', '');
+        found = language[0].split('/').join('');
     }
 
     return found;
