f8f2eff6a23d00845460a8f1552245b0daa040aa
Hamza Bahlaouane <h.bahlaouane@void.fr>
2020-07-06 10:02:04 +0100

    Container

--- packages/vactory-gatsby-ui/src/widgets/tabs/tabs.container.js
+++ packages/vactory-gatsby-ui/src/widgets/tabs/tabs.container.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import get from 'lodash.get';
 import {Wysiwyg, Tabs} from "vactory-gatsby-ui";
-import {Box} from "vactory-ui"
+import {Box, Heading} from "vactory-ui"
 
 export const TabsContainer = ({data}) => {
     const title = get(data, 'extra_field.title');
-    const description = <Wysiwyg html={get(data, 'extra_field.intro.value.#text')}/>;
+    const raw_description = get(data, 'extra_field.intro.value.#text', null);
+    const description = <Wysiwyg html={raw_description}/>;
 
     const items = data.components.map(component => {
         return {
@@ -16,8 +17,12 @@ export const TabsContainer = ({data}) => {
 
     return (
         <Box>
-            <h1>{title}</h1>
-            <div>{description}</div>
+            <Box sx={{
+                'text-align': 'center'
+            }}>
+                <Heading level={2}>{title}</Heading>
+                {raw_description.length > 0 && <div>{description}</div>}
+            </Box>
             <Box my="large">
                 <Tabs
                     items={items}
