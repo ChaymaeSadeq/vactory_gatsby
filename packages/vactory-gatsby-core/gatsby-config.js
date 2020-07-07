ada0f686ad113de19fff3932adb6b8f36765746b
Hamza Bahlaouane <h.bahlaouane@void.fr>
2020-07-07 19:54:36 +0100

    Offline: regex.

--- packages/vactory-gatsby-core/gatsby-config.js
+++ packages/vactory-gatsby-core/gatsby-config.js
@@ -13,6 +13,47 @@ module.exports = {
         `gatsby-plugin-remove-generator`,
         {
             resolve: `gatsby-plugin-offline`,
+            options: {
+                workboxConfig: {
+                    importWorkboxFrom: 'cdn',
+                    runtimeCaching: [
+                        {
+                            // Use cacheFirst since these don't need to be revalidated (same RegExp
+                            // and same reason as above)
+                            urlPattern: /(\.js$|\.css$|static\/)/,
+                            handler: `CacheFirst`,
+                        },
+                        {
+                            // page-data.json files are not content hashed
+                            urlPattern: /^https?:.*\page-data\/.*\/page-data\.json/,
+                            handler: `StaleWhileRevalidate`,
+                        },
+                        {
+                            // app-data.json is not content hashed
+                            urlPattern: /^https?:.*\/page-data\/app-data\.json/,
+                            handler: `StaleWhileRevalidate`,
+                        },
+                        {
+                            // Add runtime caching of various other page resources
+                            urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+                            handler: `StaleWhileRevalidate`,
+                        },
+                        {
+                            // Google Fonts CSS (doesn't end in .css so we need to specify it)
+                            urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
+                            handler: `StaleWhileRevalidate`,
+                        },
+                        {
+                            // HTML Pages
+                            urlPattern: /(\/\w+)*\/?$/,
+                            handler: `NetworkFirst`,
+                            options: {
+                                networkTimeoutSeconds: 1,
+                            },
+                        },
+                    ],
+                }
+            }
         },
         {
             resolve: `gatsby-plugin-react-redux`,
