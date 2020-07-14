1a5c28cfe9a7fc3d0929f4a4c9651e5646bea4e2
Hamza Bahlaouane <h.bahlaouane@void.fr>
2020-07-14 18:13:38 +0100

    PrevNext: News

--- packages/vactory-gatsby-news/src/components/post.js
+++ packages/vactory-gatsby-news/src/components/post.js
@@ -1,32 +1,46 @@
 import React from "react"
-import {SocialShare, WebShare} from "vactory-gatsby-ui"
+import {SocialShare, WebShare, Wysiwyg} from "vactory-gatsby-ui"
 import {VCC} from "vactory-gatsby-vcc"
+import {NextPre} from "vactory-gatsby-nextpre"
 import {CardNews, imageLayoutStyles, normalizeNodes, postsQueryParams} from "vactory-gatsby-news"
+import {Box, Container, Row, Col, Heading, Text} from "vactory-ui";
 
 const Post = ({post}) => {
+    const bodyMarkup = <Wysiwyg html={post.body.processed}/>;
+
     return (
         <div>
-            <main>
-                <h1>{post.title}</h1>
-                {/*<p>*/}
-                {/*    {post.date}*/}
-                {/*</p>*/}
-                {/*<div>{post.body}</div>*/}
-                <SocialShare />
-                <WebShare />
-                <VCC
-                    nid={post.drupal_internal__nid}
-                    resource={'vactory_news'}
-                    resourceType={'node--vactory_news'}
-                    queryParams={postsQueryParams}
-                    normalizer={normalizeNodes}
-                    renderNode={
-                        node =>  <CardNews {...node} imageSettings={imageLayoutStyles.threeColumns} />
-                    }
-                />
-            </main>
+            <Container>
+                <Row>
+                    <Col>
+                        <Heading level={1}>{post.title}</Heading>
+                        <Text>{post.field_vactory_date}</Text>
+                        <hr />
+                        <Box py={'15px'}>{bodyMarkup}</Box>
+                        <hr />
+                        <SocialShare/>
+                        <WebShare/>
+                    </Col>
+                </Row>
+            </Container>
+            <VCC
+                nid={post.drupal_internal__nid}
+                resource={'vactory_news'}
+                resourceType={'node--vactory_news'}
+                queryParams={postsQueryParams}
+                normalizer={normalizeNodes}
+                renderNode={
+                    node => <CardNews {...node} imageSettings={imageLayoutStyles.threeColumns}/>
+                }
+            />
+            <NextPre
+                nid={post.drupal_internal__nid}
+                resource={'vactory_news'}
+                queryParams={postsQueryParams}
+                normalizer={normalizeNodes}
+            />
         </div>
     )
-}
+};
 
 export default Post
