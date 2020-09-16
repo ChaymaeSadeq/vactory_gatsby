e8bf15c822dbc2f0efaf759a83302221731b3ac6
Abdelkoddous <a.elidrissi@void.fr>
2020-09-16 10:24:09 +0100

    Update header

--- projects/capital-azur/src/components/Header.js
+++ projects/capital-azur/src/components/Header.js
@@ -38,19 +38,6 @@ const Nav = ({asButton, active, ...props}) => {
 		}
 	}
 
-	if ( viewMode === 'desktop' ) {
-		styleProperties['&:not(:first-of-type)::before'] = {
-			content: '""',
-			display: 'block',
-			width: '1px',
-			height: '16px',
-			top: 'calc(50% - 16px/2)',
-			left: '-1px',
-			bg: 'rgba(0, 0, 0, .16)',
-			position: 'absolute',
-		}
-	}
-
 	let ButtonStyleProperties = {
 		fontWeight: 'medium',
 		bg: 'darkBlue',
@@ -63,6 +50,9 @@ const Nav = ({asButton, active, ...props}) => {
 		py: 4,
         boxShadow: '0 10px 14px -5px rgba(191, 191, 191, .67)',
 		transition: '.3s ease-in',
+		width: 'calc(100% - 24px)',
+		textAlign: 'center',
+		mt: 10,
 
 		'&:not(:first-of-type)::before': {
 			content: 'none',
@@ -73,6 +63,22 @@ const Nav = ({asButton, active, ...props}) => {
             color: 'darkBlue',
 			boxShadow: '0 10px 14px 5px rgba(191, 191, 191, .67)',
 		},
+	}
+
+	if ( viewMode === 'desktop' ) {
+		styleProperties['&:not(:first-of-type)::before'] = {
+			content: '""',
+			display: 'block',
+			width: '1px',
+			height: '16px',
+			top: 'calc(50% - 16px/2)',
+			left: '-1px',
+			bg: 'rgba(0, 0, 0, .16)',
+			position: 'absolute',
+		}
+
+		ButtonStyleProperties['width'] = 'auto';
+		ButtonStyleProperties['mt'] = 0;
 	}
 
 	if ( asButton ) {
@@ -99,12 +105,19 @@ const Navs = props => {
 			onClickOutside={props.onClickOutside} >
 			<DefaultNavs sx={{
 				flexDirection: 'column !important',
+				alignItems: 'flex-start',
 				minHeight: '100vh',
 				width: '250px',
 				backgroundColor: 'white',
 			}} {...props} />
 		</Layer>
 	);
+}
+
+const Logo = props => {
+	return <Anchor href="/" ml="small" mr="auto" {...props}>
+		<Image src={capital_azur_logo} alt="Capital Azur Logo" height="82px" display="block" />
+	</Anchor>
 }
 
 export const CapitalAzurHeader = () => {
@@ -115,11 +128,16 @@ export const CapitalAzurHeader = () => {
 	return <Header px="large" py={5} bg="white" boxShadow={1}>
 			<ViewModeProvider value={isUpLg ? 'desktop' : 'mobile'}>
 			<Container as={Flex} alignItems='center'>
-				<Anchor href="/" ml="small" mr="auto">
-					<Image src={capital_azur_logo} alt="Capital Azur Logo" height="82px" display="block" />
-				</Anchor>
+				<Logo />
 
 				<Navs menuOpened={menuOpened} onClickOutside={() => openCloseMenu(false)} className={"navs"} p={0}>
+					{ !isUpLg && <Logo sx={{
+						mx: 12,
+						mb: 20,
+						color: 'lightBlue',
+						borderBottom: '1px solid currentColor',
+						width: 'calc(100% - 24px)',
+						}} />}
 					<Nav href="/produits-services">Produits & Services</Nav>
 					<Nav href="#">Nous connaître</Nav>
 					<Nav href="#" active={true}>Insights</Nav>
