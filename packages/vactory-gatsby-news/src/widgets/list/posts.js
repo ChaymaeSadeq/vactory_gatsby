e40a50e7154a5ddde951e85f22ca9b336bdef31d
Abdelkoddous <a.elidrissi@void.fr>
2021-02-09 16:44:45 +0100

    remove react Frangement

--- packages/vactory-gatsby-news/src/widgets/list/posts.js
+++ packages/vactory-gatsby-news/src/widgets/list/posts.js
@@ -6,13 +6,12 @@ const Posts = ({ posts }) => {
   return (
 		<div className="space-y-4 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:grid-cols-3 lg:gap-8">
 			{posts.map((node) => (
-					<React.Fragment key={node.id}>
-						<NewsCard
-							{...node}
-							imageSettings={imageLayoutStyles.threeColumns}
-						/>
-					</React.Fragment>
-				))}
+				<NewsCard
+					key={node.id}
+					{...node}
+					imageSettings={imageLayoutStyles.threeColumns}
+				/>
+			))}
 		</div>
   );
 };
