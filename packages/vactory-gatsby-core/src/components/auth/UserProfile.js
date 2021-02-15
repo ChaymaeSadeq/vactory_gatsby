597821200e5fa7906a1d5de1609565e7fee4409d
Abdelkoddous <a.elidrissi@void.fr>
2021-02-15 18:14:59 +0100

    userprofile avatar

--- packages/vactory-gatsby-core/src/components/auth/UserProfile.js
+++ packages/vactory-gatsby-core/src/components/auth/UserProfile.js
@@ -1,61 +1,35 @@
-import React, {useState, useRef} from 'react'
-import {Box, Flex, Drop, Anchor} from 'vactory-ui'
+import React from 'react'
+import {Link, Dropdown} from 'vactory-gatsby-ui'
 import {useTranslation} from "react-i18next"
 
 const DefaultAvatar = ({name}) => {
     return (
-        <Box bg={'primary400'} sx={{
-            padding: '10px',
-            borderRadius: '50%',
-            marginRight: '10px',
-            width: '37px',
-            height: '37px'
-        }}>
+        <div className="bg-indigo-400 p-2.5 rounded-full ltr:mr-2.5 rtl:ml-2.5 w-9 h-9">
             {name}
-        </Box>
+        </div>
     )
 };
 
 const UserAvatarImage = (props) => {
     return (
-        <Box
-            as='img'
-            bg={'primary400'}
-             sx={{
-            borderRadius: '50%',
-            marginRight: '10px',
-            width: '37px',
-            height: '37px',
-        }} {...props} />
-    )
+		<img
+			className="inline-block h-10 w-10 rounded-full bg-indigo-400 bg-opacity-50 ltr:mr-2.5 rtl:ml-2.5"
+			{...props}
+		/>
+	);
 };
 
-const DropLink = ({children, hasBorder = true, ...rest}) => {
+const DropLink = ({props}) => {
     return (
-        <Anchor
-            px="small"
-            py="medium"
-            borderBottom={hasBorder ? '1px solid' : null}
-            borderBottomColor={hasBorder ? 'gray300' : null}
-            sx={{
-                cursor: 'pointer',
-                ':hover': {
-                    backgroundColor: 'primary400',
-                    color: 'white',
-                }
-            }}
-            {...rest}
-        >
-            {children}
-        </Anchor>
-    )
+        <Link
+            className="hover:text-indigo-400 text-indigo-600"
+            {...props}
+        />
+	);
 }
 
 export const UserProfile = ({logout, edit, user}) => {
     const {t} = useTranslation();
-
-    const [showDrop, setShowDrop] = useState(false);
-    const targetRef = useRef();
 
     let UserAvatar = () => <DefaultAvatar name={user.name_initial}/>
     if (user.avatar) {
@@ -63,47 +37,32 @@ export const UserProfile = ({logout, edit, user}) => {
     }
 
     return (
-        <Box>
-            <Flex
-                ref={targetRef}
-                onClick={() => setShowDrop(!showDrop)}
-                alignItems='center'
-                justifyContent='center'
-                sx={{
-                    cursor: 'pointer',
-                    padding: '5px 15px',
-                    background: '#3c3c3c',
-                    color: '#FFF',
-                    borderRadius: '25px',
-                    marginLeft: '10px'
-                }}>
-                <UserAvatar />
-                <Box>{user.full_name}</Box>
-            </Flex>
-            {showDrop && <Drop sx={{overflow: 'initial'}}
-                               align={{top: 'bottom', left: 'left'}} target={targetRef.current} stretch={true}>
-                <Flex sx={{
-                    border: '1px solid',
-                    borderColor: 'gray200',
-                    backgroundColor: 'white',
-                    boxShadow: 3,
-                    flexDirection: 'column'
-                }}>
-                    <DropLink
-                        target="_blank"
-                        href={edit}>
-                        {t('Gérer votre compte')}
-                    </DropLink>
-
-                    <DropLink
-                        onClick={logout}
-                        href={'#.'}
-                        hasBorder={false}
-                    >
-                        {t('Se déconnecter')}
-                    </DropLink>
-                </Flex>
-            </Drop>}
-        </Box>
-    )
+		<div>
+			<Dropdown
+				label={
+					<div className="flex border rounded-md">
+						<UserAvatar />
+						<p>{user.full_name}</p>
+					</div>
+				}
+				items={
+					<ul className="flex flex-col space-y-2">
+						<li>
+							<DropLink target="_blank" href={edit}>
+								{t("Gérer votre compte")}
+							</DropLink>
+						</li>
+						<li>
+							<DropLink
+								onClick={logout}
+								href={"#."}
+							>
+								{t("Se déconnecter")}
+							</DropLink>
+						</li>
+					</ul>
+				}
+			/>
+		</div>
+	);
 };
